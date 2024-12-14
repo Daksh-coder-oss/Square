d84bee9b668ada0d109487e8cf1f54654b47df59
Daksh-coder-oss <vv0039190@gmail.com>
2024-12-14 10:17:39 +0000

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().setup(800,800)
+turtle.Screen().bgcolor("Yellow")
+polygon=turtle.Turtle()
+for i in range(5):
+    polygon.forward(100)
+    polygon.right(90)
+turtle.done()    
